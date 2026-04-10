dd6ba4ea379c9adef5cfa9398dad1ef557f6201c
jelle284 <33733782+jelle284@users.noreply.github.com>
2026-04-10 23:21:47 +0200

    Update copy_files.bat

--- misc/copy_files.bat
+++ misc/copy_files.bat
@@ -1,2 +1,2 @@
-xcopy "%~dp0macro\*.FCMacro" "%APPDATA%\FreeCAD\v1.1\Macro\" /Y
-xcopy "%~dp0scripts\*.py" "%APPDATA%\FreeCAD\v1.1\Macro\" /Y
+xcopy "%~dp0\..\macro\*.FCMacro" "%APPDATA%\FreeCAD\v1-1\Macro\" /Y
+xcopy "%~dp0\..\scripts\*.py" "%APPDATA%\FreeCAD\v1-1\Macro\" /Y
